75bd4d0d5ce158bb637cf9c8b7b510769476cd6e
Altoro <sashadorodnov12@gmail.com>
2024-11-03 16:24:44 +0200

    0.1.1

--- Zombie Shooter/Assets/Scripts/PlayerInput.cs
+++ Zombie Shooter/Assets/Scripts/PlayerInput.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    public event Action OnTouch;
+    //public event Action OnTouch;
     private void Update()
     {
-         if (Input.GetMouseButtonDown(0))
-         {
-             OnTouch?.Invoke();
-             Debug.Log(OnTouch);
-         }
+        //  if (Input.GetMouseButtonDown(0))
+        //  {
+        //      OnTouch?.Invoke();
+        //      Debug.Log(OnTouch);
+        //  }
     }
 }
--- Zombie Shooter/Assets/Scripts/PlayerMove.cs
+++ Zombie Shooter/Assets/Scripts/PlayerMove.cs
@@ -1,29 +1,32 @@
-using Unity.VisualScripting;
+using DG.Tweening;
 using UnityEngine;
 using Zenject;
 public class PlayerMove : MonoBehaviour
 {
     [Inject] private PlayerInput _playerInput;
     private Rigidbody2D _rigidBody;
-    private float _moveSpeed = 2f;
+    private float _moveSpeed = 2.5f;
     private Vector3 _touchPosition, whereToMove;
     private bool _isMoving = false;
     private float _previousDistanceToTouchPos, _curretDistanceToTouchPos;
+    private float _rotationAngle;
     
 
     private void Start()
     {
         _rigidBody = gameObject.GetComponent<Rigidbody2D>();
     }
-    private void OnEnable()
-    {
-        _playerInput.OnTouch += Move;
-    }
-    private void FixedUpdate()
+    // private void OnEnable()
+    // {
+    //     _playerInput.OnTouch += Move;
+    // }
+    private void Update()
     {
         if (_isMoving)
             _curretDistanceToTouchPos = (_touchPosition - transform.position).magnitude;
 
+        if (Input.GetMouseButtonDown(0))
+            Move();
         if (_curretDistanceToTouchPos > _previousDistanceToTouchPos)
         {
             _isMoving = false;
@@ -32,10 +35,12 @@ public class PlayerMove : MonoBehaviour
 
         if (_isMoving)
             _previousDistanceToTouchPos = (_touchPosition - transform.position).magnitude;
+
+            //_rotationAngle = Mathf.Atan2(whereToMove.y, whereToMove.x) * Mathf.Rad2Deg;
+            //transform.rotation = Quaternion.Euler(0, 0, _rotationAngle);
     }
     private void Move()
     {
-        Debug.Log(nameof(Move));
         _previousDistanceToTouchPos = 0;
         _curretDistanceToTouchPos = 0;
         _isMoving = true;
@@ -43,5 +48,8 @@ public class PlayerMove : MonoBehaviour
         _touchPosition.z = 0;
         whereToMove = (_touchPosition - transform.position).normalized;
         _rigidBody.velocity = new Vector2(whereToMove.x * _moveSpeed, whereToMove.y * _moveSpeed); 
+
+        _rotationAngle = Mathf.Atan2(whereToMove.y, whereToMove.x) * Mathf.Rad2Deg;
+        transform.DORotate(new Vector3(0, 0, _rotationAngle), 0.3f);
     }
 }
