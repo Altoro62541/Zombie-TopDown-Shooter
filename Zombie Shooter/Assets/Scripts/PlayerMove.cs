ba5c558008bc9fdb67a2f9cc6054e6795fbc2328
Altoro <sashadorodnov12@gmail.com>
2024-11-04 21:04:41 +0200

    0.1.2

--- Zombie Shooter/Assets/Scripts/PlayerMove.cs
+++ Zombie Shooter/Assets/Scripts/PlayerMove.cs
@@ -1,55 +1,52 @@
-using DG.Tweening;
 using UnityEngine;
-using Zenject;
 public class PlayerMove : MonoBehaviour
 {
-    [Inject] private PlayerInput _playerInput;
-    private Rigidbody2D _rigidBody;
-    private float _moveSpeed = 2.5f;
-    private Vector3 _touchPosition, whereToMove;
-    private bool _isMoving = false;
-    private float _previousDistanceToTouchPos, _curretDistanceToTouchPos;
-    private float _rotationAngle;
-    
+private Rigidbody2D _rigidBody;
+private float _moveSpeed = 2.5f;
+private Vector2 _direction;
+private Vector3 _targetPosition;
+private bool _isMoving = false;
+private float _rotationSpeed = 360f; // Скорость поворота в градусах в секунду
+private float _targetAngle;
 
-    private void Start()
-    {
-        _rigidBody = gameObject.GetComponent<Rigidbody2D>();
-    }
-    // private void OnEnable()
-    // {
-    //     _playerInput.OnTouch += Move;
-    // }
-    private void Update()
-    {
-        if (_isMoving)
-            _curretDistanceToTouchPos = (_touchPosition - transform.position).magnitude;
+private void Start()
+{
+    _rigidBody = gameObject.GetComponent<Rigidbody2D>();
+}
 
-        if (Input.GetMouseButtonDown(0))
-            Move();
-        if (_curretDistanceToTouchPos > _previousDistanceToTouchPos)
+private void Update()
+{
+    if (_isMoving)
+    {
+        _direction = (_targetPosition - transform.position).normalized;
+        _rigidBody.velocity = _direction * _moveSpeed;
+
+        _targetAngle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
+        Quaternion targetRotation = Quaternion.Euler(0, 0, _targetAngle);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, _targetPosition) <= 0.1f)
         {
-            _isMoving = false;
-            _rigidBody.velocity = Vector2.zero;
+            StopMovement();
         }
-
-        if (_isMoving)
-            _previousDistanceToTouchPos = (_touchPosition - transform.position).magnitude;
-
-            //_rotationAngle = Mathf.Atan2(whereToMove.y, whereToMove.x) * Mathf.Rad2Deg;
-            //transform.rotation = Quaternion.Euler(0, 0, _rotationAngle);
     }
-    private void Move()
+
+    if (Input.GetMouseButtonDown(0))
     {
-        _previousDistanceToTouchPos = 0;
-        _curretDistanceToTouchPos = 0;
-        _isMoving = true;
-        _touchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        _touchPosition.z = 0;
-        whereToMove = (_touchPosition - transform.position).normalized;
-        _rigidBody.velocity = new Vector2(whereToMove.x * _moveSpeed, whereToMove.y * _moveSpeed); 
-
-        _rotationAngle = Mathf.Atan2(whereToMove.y, whereToMove.x) * Mathf.Rad2Deg;
-        transform.DORotate(new Vector3(0, 0, _rotationAngle), 0.3f);
+        StartMoving();
     }
+}
+
+private void StartMoving()
+{
+    _targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    _targetPosition.z = 0;
+    _isMoving = true;
+}
+
+private void StopMovement()
+{
+    _isMoving = false;
+    _rigidBody.velocity = Vector2.zero;
+}
 }
