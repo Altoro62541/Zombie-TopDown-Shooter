b63c4af77d1997393c4e99b7c12d3d1940783873
Maxim <36982547+Siphoin@users.noreply.github.com>
2024-11-07 13:31:54 +0500

    import Odin Inspector

--- Zombie Shooter/Assets/Scripts/ZombieEntity/SO/ZombieData.cs
+++ Zombie Shooter/Assets/Scripts/ZombieEntity/SO/ZombieData.cs
@@ -5,10 +5,7 @@ namespace Assets.Scripts.ZombieEntity.SO
 {
     public class ZombieData : ScriptableObject
     {
-        [MenuItem("Tools/MyTool/Do It in C#")]
-        static void DoIt()
-        {
-            EditorUtility.DisplayDialog("MyTool", "Do It in C# !", "OK", "");
-        }
+        [SerializeField] private float _health;
+        [SerializeField] private float _speedMove;
     }
 }
