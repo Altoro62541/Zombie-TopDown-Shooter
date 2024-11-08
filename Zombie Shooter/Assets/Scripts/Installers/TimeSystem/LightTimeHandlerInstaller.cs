ac1b6b78bfb841acf8f2c0c0906b4f80c330e2d1
Maxim <36982547+Siphoin@users.noreply.github.com>
2024-11-08 16:13:51 +0500

    redirect LightTimeHandler to installer

---
+++ Zombie Shooter/Assets/Scripts/Installers/TimeSystem/LightTimeHandlerInstaller.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Zenject;
+using ZombieShooter.TimeSystem;
+
+namespace ZombieShooter.Installers.TimeSystem
+{
+    public class LightTimeHandlerInstaller : MonoInstaller
+    {
+        [SerializeField] private LightTimeHandler _prefab;
+
+        public override void InstallBindings()
+        {
+            Container.InstantiatePrefab(_prefab);
+        }
+    }
+}
