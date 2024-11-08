2863b4d8a1905c1b74b16bf03848df4974085d04
Maxim <36982547+Siphoin@users.noreply.github.com>
2024-11-08 15:17:39 +0500

    fix Catching UI on PlayerMovement

---
+++ Zombie Shooter/Assets/Scripts/UI/Installers/EventSystemInstaller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Zenject;
+
+namespace ZombieShooter.UI.Installers
+{
+    public class EventSystemInstaller : MonoInstaller
+    {
+
+        [SerializeField] private EventSystem _prefab;
+
+        public override void InstallBindings()
+        {
+            if (!_prefab)
+            {
+                throw new NullReferenceException("event system prefab are not assigned");
+            }
+
+            var prefab = Container.InstantiatePrefabForComponent<EventSystem>(_prefab);
+
+            Container.Bind<EventSystem>().FromInstance(prefab);
+        }
+    }
+}
