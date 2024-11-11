c18593499cdae7f9b1ce3a18a8924dc961ef5055
Altoro <sashadorodnov12@gmail.com>
2024-11-11 19:08:29 +0200

    0.1.8

--- Zombie Shooter/Assets/Scripts/Spawn/ZombieSpawner.cs
+++ Zombie Shooter/Assets/Scripts/Spawn/ZombieSpawner.cs
@@ -1,13 +1,14 @@
 using Cysharp.Threading.Tasks;
 using System;
+using System.Linq;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 using Zenject;
 using ZombieShooter.Factories;
 using ZombieShooter.PlayerEntity;
 using ZombieShooter.Spawn.SO;
+using ZombieShooter.TimeSystem;
 using ZombieShooter.ZombieEntity;
-
+using ZombieShooter.ZombieEntity.SO;
 namespace ZombieShooter.Spawn
 {
     public class ZombieSpawner : MonoBehaviour
@@ -15,11 +16,23 @@ namespace ZombieShooter.Spawn
         [Inject] private IZombieFactory _factory;
         [Inject] private IWorld _world;
         [Inject] private IPlayer _player;
-        [SerializeField] private Zombie _zombie;
+        [Inject] private IDayCounterHandler _dayHandler;
         [SerializeField] private ZombieSpawnSettings _spawnSettings;
+        [SerializeField] private ZombieSpawnerConfig _spawnConfigs;
+        [SerializeField] private Zombie[] _zombies;
         public void Start()
         {
             Spawning().Forget();
+            RebuildZombiesArray(); 
+        }
+        private void OnEnable()
+        {
+            _dayHandler.OnNewDay += RebuildZombiesArray;
+            RebuildZombiesArray(); 
+        }
+        private void OnDisable()
+        {
+            _dayHandler.OnNewDay -= RebuildZombiesArray;
         }
         private async UniTask Spawning()
         {
@@ -29,12 +42,20 @@ namespace ZombieShooter.Spawn
                 await UniTask.Delay(timeSpan); 
 
                 Vector3 position = _world.GetRandomTilePosition(_player.Position, _spawnSettings.SpawnRadius);
-
+                int randomZombie = UnityEngine.Random.Range(0, _zombies.Length);
                 if (position != Vector3.zero)
                 {
-                    _factory.Create(_zombie, position, Quaternion.identity, null);
+                    _factory.Create(_zombies[randomZombie], position, Quaternion.identity, null);
                 }
             }
+        }
+        private void RebuildZombiesArray()
+        {
+        long currentDay = _dayHandler.CurrentDay;
+        _zombies = _spawnConfigs.Elements
+            .Where(config => config.Day <= currentDay) 
+            .Select(config => config.Prefab) 
+            .ToArray();
         }
 
     }
