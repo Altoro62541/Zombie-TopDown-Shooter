6c9e1184aa015793f12c3fd0a730ed8a78b9101e
Maxim <36982547+Siphoin@users.noreply.github.com>
2024-11-11 19:45:18 +0500

    fix Weapon View

--- Zombie Shooter/Assets/Scripts/UI/WeaponView.cs
+++ Zombie Shooter/Assets/Scripts/UI/WeaponView.cs
@@ -16,34 +16,34 @@ namespace ZombieShooter.UI
         [SerializeField] private TextMeshProUGUI _ammoText;
          private CompositeDisposable _disposable = new();
 
-
-        private void Awake()
-        {
-            _weaponImage = GetComponent<Image>();
-            _weaponText = GetComponent<TextMeshProUGUI>();
-            _ammoText = GetComponent<TextMeshProUGUI>();
-            UpdateView();
-        }
         private void OnEnable()
         {
-            // _weaponHandler.OnEquip.Subscribe(weapon => {
-            //     UpdateView();
-            //    weapon.OnShoot += UpdateView;
-            // }).AddTo(_disposable);
+            _weaponHandler.OnEquip.Subscribe(weapon => {
+                 UpdateView();
+               weapon.OnShoot += UpdateView;
+                weapon.OnReloadFinish += UpdateView;
+            }).AddTo(_disposable);
 
         }
         private void OnDestroy()
         {
             _disposable.Clear();
-            _weaponHandler.ActiveWeapon.OnShoot -= UpdateView;
+            try
+            {
+                _weaponHandler.ActiveWeapon.OnShoot -= UpdateView;
+                _weaponHandler.ActiveWeapon.OnReloadFinish -= UpdateView;
+            }
+            catch
+            {
+            }
+            
         }
         private void UpdateView()
         {
-            Debug.Log(_weaponHandler.ActiveWeapon);
             _weaponImage.sprite = _weaponHandler.ActiveWeapon.Icon;
             _weaponImage.SetNativeSize();
-            _weaponText.text = $"Weapon: {_weaponHandler.ActiveWeapon.Name}";
-            _ammoText.text = $"Bullets: {_weaponHandler.ActiveWeapon.CurrentAmmunition}/{_weaponHandler.ActiveWeapon.AmmunitionBank}";
+            _weaponText.text = _weaponHandler.ActiveWeapon.Name;
+            _ammoText.text = $"{_weaponHandler.ActiveWeapon.CurrentAmmunition}/{_weaponHandler.ActiveWeapon.AmmunitionBank}";
         }
     }
 }
