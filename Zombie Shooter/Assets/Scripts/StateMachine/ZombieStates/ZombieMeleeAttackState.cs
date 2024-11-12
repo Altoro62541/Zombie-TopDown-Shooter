d5b10809becc18a9d4aea5e2089064d88468734a
Maxim <36982547+Siphoin@users.noreply.github.com>
2024-11-12 16:09:42 +0500

    turn off attacking and moving to player if dead

--- Zombie Shooter/Assets/Scripts/StateMachine/ZombieStates/ZombieMeleeAttackState.cs
+++ Zombie Shooter/Assets/Scripts/StateMachine/ZombieStates/ZombieMeleeAttackState.cs
@@ -27,17 +27,25 @@ namespace ZombieShooter.StateMachine.ZombieStates
             Target.Despawn.IsActive = false;
             Target.AI.Stop();
             _cancellationTokenSource = new();
+            _player.HeathComponent.OnDead += OnDeadPlayer;
             Attack().Forget();
         }
 
         public override void Exit()
         {
+            _player.HeathComponent.OnDead -= OnDeadPlayer;
             _cancellationTokenSource?.Cancel();
         }
 
         public void SetData(IPlayer player)
         {
             _player = player;
+        }
+
+        private void OnDeadPlayer()
+        {
+            _player.HeathComponent.OnDead -= OnDeadPlayer;
+            Target.StateMachine.TurnIdle();
         }
 
         public void OnFixedUpdate()
